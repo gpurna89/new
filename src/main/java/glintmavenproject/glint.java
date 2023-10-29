110c4de0e6f01b77d8842bfb7103f4e56e3f31c8
gpurna89 <g.purna89@gmail.com>
2023-10-29 09:56:08 +0530

    glint

---
+++ src/main/java/glintmavenproject/glint.java
@@ -0,0 +1,15 @@
+package glintmavenproject;
+
+
+
+public class glint {
+	
+	public static void main(String[] args) {
+		
+	
+	
+	file loc = new file ("C:\\Users\\M DHANISHKUMAR\\Desktop");
+
+}
+	
+}
